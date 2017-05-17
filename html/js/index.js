31e90ee14fe12b765ac03d0a1785d2975e47e653
weiningning <526365416@qq.com>
2017-05-17 21:29:27 +0800

    5.17

---
+++ html/js/index.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+	    autoplay:3000,//可选选项，自动滑动
+		pagination : '.swiper-pagination',
+		paginationClickable :true,
+		prevButton:'.swiper-button-prev',
+        nextButton:'.swiper-button-next',
+        loop:true,
+})
